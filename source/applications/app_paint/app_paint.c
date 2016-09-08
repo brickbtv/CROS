e1f476e2f709ec6606a6f833b9dae6b2a63f03d3
brick <brickbtv@gmail.com>
2016-09-08 21:48:08 +0300

    cursor for brushes

--- source/applications/app_paint/app_paint.c
+++ source/applications/app_paint/app_paint.c
@@ -17,6 +17,7 @@ Canvas * paint_canvas;
 bool paint_run = true;
 int state = 0;
 short * map;
+short * map_brushes;
 unsigned int width, height;
 static bool blink = true;
 
@@ -26,9 +27,13 @@ typedef struct PCursor{
 }PCursor;
 
 PCursor pcur, prevcur;
+PCursor bpcur, bprevcur;
 
 int paint_y_offset = 2;
 int paint_x_offset = 1;
+
+int paint_brush_y_offset = 2;
+int paint_brush_x_offset = 1;
 
 bool open_file(const char * path){
 	FILE * file = fs_open_file(path, 'r');
@@ -39,25 +44,26 @@ bool open_file(const char * path){
 	}
 }
 
-void draw_blink(){
+void draw_blink(short * palette, PCursor * c, PCursor * prevc, int xoffset, int yoffset, int wid){
 	short * canvas = paint_canvas->addr;
-	char sel_ch = (char)map[pcur.x + pcur.y * width];
+	char sel_ch = (char)palette[c->x + c->y * wid];
 	
-	short origin_char = map[pcur.x + pcur.y * width];
+	short origin_char = palette[c->x + c->y * wid];
 	
 	char txt = origin_char >> 12;
 	char bg = origin_char >> 8;
 		
 	short sel_ch_colored = bg << 12 | txt << 8 | sel_ch;
 	
-	*(canvas + (prevcur.y + paint_y_offset) * paint_canvas->res_hor + prevcur.x + paint_x_offset) = map[prevcur.x + prevcur.y * width];
-	*(canvas + (pcur.y + paint_y_offset) * paint_canvas->res_hor + pcur.x + paint_x_offset) = blink?sel_ch_colored:map[pcur.x + pcur.y * width];
+	*(canvas + (prevc->y + yoffset) * paint_canvas->res_hor + prevc->x + xoffset) = palette[prevc->x + prevc->y * wid];
+	*(canvas + (c->y + yoffset) * paint_canvas->res_hor + c->x + xoffset) = blink?sel_ch_colored:palette[c->x + c->y * wid];
 }
 
 void paintBlinkCBack(unsigned int tn){
 	if (tn == 2){
 		blink = ! blink;
-		draw_blink();
+		draw_blink(map, &pcur, &prevcur, paint_x_offset, paint_y_offset, width);
+		draw_blink(map_brushes, &bpcur, &bprevcur, paint_brush_x_offset, paint_brush_y_offset, 32);
 	}
 }
 
@@ -75,6 +81,43 @@ void draw_brushes(){
 	}
 }
 
+void paintUpdateCursors(int value, char u, char d, char l, char r, int max_width, int max_height, PCursor * c, PCursor * prev){
+	if (value == KEY_UP || value == KEY_DOWN || value == KEY_LEFT || value == KEY_RIGHT)
+		blink = true;
+	
+	if (value == u){
+		if (c->y > 0){
+			prev->x = c->x;
+			prev->y = c->y;
+			c->y--;
+		}
+	}
+	
+	if (value == d){
+		if (c->y < max_height - 1){
+			prev->x = c->x;
+			prev->y = c->y;
+			c->y++; 
+		}
+	}
+	
+	if (value == l){
+		if (c->x > 0){
+			prev->x = c->x;
+			prev->y = c->y;
+			c->x--;
+		}
+	}
+	
+	if (value == r){
+		if (c->x < max_width - 1){
+			prev->x = c->x;
+			prev->y = c->y;
+			c->x++; 
+		}
+	}
+}
+
 void appPaintMsgHandler(int type, int reason, int value){
 	switch (type){
 		case SDK_PRC_MESSAGE_CLK:
@@ -88,42 +131,11 @@ void appPaintMsgHandler(int type, int reason, int value){
 					}
 				}
 				
-				if (value == KEY_UP || value == KEY_DOWN || value == KEY_LEFT || value == KEY_RIGHT)
-					blink = true;
-				
-				if (value == KEY_UP){
-					if (pcur.y > 0){
-						prevcur.x = pcur.x;
-						prevcur.y = pcur.y;
-						pcur.y--;
-					}
-				}
-				
-				if (value == KEY_DOWN){
-					if (pcur.y < height - 1){
-						prevcur.x = pcur.x;
-						prevcur.y = pcur.y;
-						pcur.y++; 
-					}
-				}
-				
-				if (value == KEY_LEFT){
-					if (pcur.x > 0){
-						prevcur.x = pcur.x;
-						prevcur.y = pcur.y;
-						pcur.x--;
-					}
-				}
-				
-				if (value == KEY_RIGHT){
-					if (pcur.x < width - 1){
-						prevcur.x = pcur.x;
-						prevcur.y = pcur.y;
-						pcur.x++; 
-					}
-				}				
+				paintUpdateCursors(value, 'w', 's', 'a', 'd', width, height, &pcur, &prevcur);
+				paintUpdateCursors(value, KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, 32, 8, &bpcur, &bprevcur);
 
-				draw_blink();
+				draw_blink(map, &pcur, &prevcur, paint_x_offset, paint_y_offset, width);
+				draw_blink(map_brushes, &bpcur, &bprevcur, paint_brush_x_offset, paint_brush_y_offset, 32);
 			}
 			break;
 	}
@@ -134,20 +146,32 @@ void app_paint(const char * path){
 	
 	paint_canvas = (Canvas *)sdk_prc_getCanvas();
 		
+	paint_brush_x_offset = paint_canvas->res_hor - 34 + 1;
+		
 	pcur.x = 0;
 	pcur.y = 0;
 	
 	prevcur.x = 0;
 	prevcur.y = 0;
 	
+	bpcur.x = 0;
+	bpcur.y = 0;
+	
+	bprevcur.x = 0;
+	bprevcur.y = 0;
+	
 	width = 20;
 	height = 10;
 	map = calloc(width * height * sizeof(short));
+	map_brushes = calloc(256 * sizeof(short));
 	
 	short back_ch = SCR_COLOR_BLUE << 12 | SCR_COLOR_WHITE << 8 | ',';
 	
 	for (int i = 0; i < width * height; i++)
 		map[i] = back_ch;
+		
+	for (short i = 0; i < 256; i++)
+		map_brushes[i] = SCR_COLOR_BLACK << 12 | SCR_COLOR_WHITE << 8 | i;
 	
 	sdk_scr_clearScreen(paint_canvas, SCR_COLOR_BLACK);
 	//sdk_scr_printf(paint_canvas, "WOoops");
