8871438e1b9d59ac101919d9584c3ebe5b1f87be
Vlad Shvets <v.shvetz@cnpo.ru>
2017-04-10 18:09:25 +0300

    editor: reading routine upd

--- source/applications/app_texteditor/preprocessor/preprocessor.c
+++ source/applications/app_texteditor/preprocessor/preprocessor.c
@@ -4,56 +4,61 @@
 #include <details/memdetails.h>
 #include <string_shared.h>
 
-list_t * preprocess_file(const char* path, int screenWidth){
-	/*
+#define DEF_BUF_SIZE 256
+
+void preprocess_oneliner(list_t * text_lines, char * buf, int screenWidth);
+void preprocess_multiliner(list_t * text_lines, char * buf, int screenWidth, int pos);
+
+/*
 		Preprocessing split input file for \n symbols.
 		Result stores in list.
-	*/
-	
+*/
+list_t * preprocess_file(const char* path, int screenWidth){
 	list_t* text_lines = list_new();
 	
-	// step 1
-	#define DEF_BUF_SIZE 256
 	char buf[DEF_BUF_SIZE];
 	memset(buf, 0, sizeof(char) * DEF_BUF_SIZE);
 	unsigned int rb;
 	
 	FILE * file = fs_open_file(path, 'r');
 	
-	if (file){
-		do{	
-			int res = fs_read_file(file, buf, DEF_BUF_SIZE, &rb);
-			if (res != FS_OK){
-				list_destroy(text_lines);
-				return FALSE;
-			}
+	do{	
+		int res = fs_read_file(file, buf, DEF_BUF_SIZE, &rb);
+		if (res != FS_OK){
+			list_destroy(text_lines);
+			return 0;
+		}
+		
+		int pos = find(buf, '\n', 0);
+		
+		if (pos == -1)
+			preprocess_oneliner(text_lines, buf, screenWidth);
+		else 
+			preprocess_multiliner(text_lines, buf, screenWidth, pos);
 			
-			// splitting
-			int old_pos = -1;
-			int pos = find(buf, '\n', 0);
-			
-			if (pos == -1){
-				char* oneline = (char*)calloc(screenWidth);
-				strcpy(oneline, buf);
-				list_node_t* onl_node = list_node_new(oneline);
-				list_rpush(text_lines, onl_node);
-			}
-			
-			while (pos != -1){				
-				char* line = (char*) calloc(screenWidth);
-				strncpy(line, &buf[old_pos + 1], pos - old_pos - 1);
-				
-				list_rpush(text_lines, list_node_new(line));
-				
-				old_pos = pos;
-				pos = find(buf, '\n', pos + 1);
-			}
-		} while(rb == DEF_BUF_SIZE);
-	} else {
-		return 0;
-	}
-	
+	} while(rb == DEF_BUF_SIZE);
+
 	return text_lines;
+}
+
+void preprocess_oneliner(list_t * text_lines, char * buf, int screenWidth){
+	char* oneline = (char*)calloc(screenWidth);
+	strcpy(oneline, buf);
+	list_node_t* onl_node = list_node_new(oneline);
+	list_rpush(text_lines, onl_node);
+}
+
+void preprocess_multiliner(list_t * text_lines, char * buf, int screenWidth, int pos){
+	int old_pos = -1;
+	while (pos != -1){				
+		char* line = (char*) calloc(screenWidth);
+		strncpy(line, &buf[old_pos + 1], pos - old_pos - 1);
+		
+		list_rpush(text_lines, list_node_new(line));
+		
+		old_pos = pos;
+		pos = find(buf, '\n', pos + 1);
+	}
 }
 
 void close_preprocessed_file(list_t * text_lines){
