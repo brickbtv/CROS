209c89ee78e18193aa8483ee2c315f0757269114
Vlad Shvets <v.shvetz@cnpo.ru>
2017-04-11 13:54:08 +0300

    editor: readline impl for big files

--- source/applications/app_texteditor/preprocessor/preprocessor.c
+++ source/applications/app_texteditor/preprocessor/preprocessor.c
@@ -3,11 +3,13 @@
 #include <utils/filesystem/filesystem.h>
 #include <details/memdetails.h>
 #include <string_shared.h>
+#include <sdk/os/debug.h>
 
-#define DEF_BUF_SIZE 256
+#define DEF_BUF_SIZE 80
 
 void preprocess_oneliner(list_t * text_lines, char * buf, int screenWidth);
 void preprocess_multiliner(list_t * text_lines, char * buf, int screenWidth, int pos);
+int read_line(FILE * file, char * buf, int size);
 
 /*
 		Preprocessing split input file for \n symbols.
@@ -23,7 +25,7 @@ list_t * preprocess_file(const char* path, int screenWidth){
 	FILE * file = fs_open_file(path, 'r');
 	
 	do{	
-		int res = fs_read_file(file, buf, DEF_BUF_SIZE, &rb);
+		/*int res = fs_read_file(file, buf, DEF_BUF_SIZE, &rb);
 		if (res != FS_OK){
 			list_destroy(text_lines);
 			return 0;
@@ -34,11 +36,37 @@ list_t * preprocess_file(const char* path, int screenWidth){
 		if (pos == -1)
 			preprocess_oneliner(text_lines, buf, screenWidth);
 		else 
-			preprocess_multiliner(text_lines, buf, screenWidth, pos);
+			preprocess_multiliner(text_lines, buf, screenWidth, pos);*/
+		memset(buf, 0, sizeof(char) * DEF_BUF_SIZE);
+		rb = read_line(file, buf, DEF_BUF_SIZE);
+		char* oneline = (char*)calloc(screenWidth);
+		strncpy(oneline, buf, screenWidth);
+		list_node_t* onl_node = list_node_new(oneline);
+		list_rpush(text_lines, onl_node);
 			
-	} while(rb == DEF_BUF_SIZE);
+	} while(rb > 0);
 
 	return text_lines;
+}
+
+char read_char(FILE * file){
+	char buf[1];
+	int rb = 0;
+	int res = fs_read_file(file, buf, 1, &rb);
+	if (res != FS_OK || rb == 0)
+		return 0;
+	return buf[0];
+}
+
+int read_line(FILE * file, char * buf, int size){
+	char c;
+	int count = 0;
+	while ((c = read_char(file)) && count < size){
+		buf[count] = c;
+		count++;
+	}
+	sdk_debug_logf("%s", buf);
+	return count;
 }
 
 void preprocess_oneliner(list_t * text_lines, char * buf, int screenWidth){
